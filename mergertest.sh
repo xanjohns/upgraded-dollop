0e927c03f77438069e8ac1718a8296bee5baa5a3
Xan Johnson <xanjohns@gmail.com>
2021-07-19 21:39:07 -0600

    reverted period

--- mergertest.sh
+++ mergertest.sh
@@ -1,7 +1,7 @@
 git checkout -b add-common-config
 git subtree add --prefix third_party/common-config https://github.com/ryancj14/common-config.git main --squash
 
-mv third_party/common-config/formatter-files/.* ./
+mv third_party/common-config/formatter-files/.* .
 mv third_party/common-config/LICENSE .
 mv third_party/common-config/docs/* ./docs
 mv third_party/common-config/.github/ISSUE_TEMPLATE/* ./.github/ISSUE_TEMPLATE
