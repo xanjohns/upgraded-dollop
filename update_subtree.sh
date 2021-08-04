c5eec4707914787f23c37cd638421170958e04ff
Xan Johnson <xanjohns@gmail.com>
2021-08-03 19:48:46 -0600

    Squashed 'third_party/common-config/' content from commit 95da5f4
    git-subtree-dir: third_party/common-config
    git-subtree-split: 95da5f49e32e7d669943b0267496e2986ddfcfed

---
+++ update_subtree.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Copyright (C) 2020  The SymbiFlow Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier:	ISC
+
+git subtree pull --prefix third_party/common-config https://github.com/SymbiFlow/symbiflow-common-config.git --squash
+
